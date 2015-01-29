dc4746c5ecba3d7342613fa119da06ee86065daa
rekhubs <rek01@126.com>
2015-01-30 00:44:40 +0800

    Update webhook-test.user.js

--- webhook-test.user.js
+++ webhook-test.user.js
@@ -1,2 +1,8 @@
+// ==UserScript==
+// @name        webhook-test
+// @include     http://music.douban.com/subject/*
+// @version     0.1
+// @grant       none
+// ==/UserScript==
 
 console.log("...............");
